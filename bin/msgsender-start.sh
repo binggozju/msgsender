ed1c0216b03a367d8800937067ff0d44b64a2984
binggo <binggozhan@163.com>
2016-07-26 10:15:46 +0800

    split the configuration file and update msgsender-start.sh

--- bin/msgsender-start.sh
+++ bin/msgsender-start.sh
@@ -4,13 +4,39 @@
 #	start msgsender service
 # ---------------------------------------------------------------------
 
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 local|pre-release|production"
+	exit 1
+fi
+
+ENV=$1
+case $ENV in
+	local|pre-release|production)
+	;;
+
+	*)
+	echo "Error: unvalid parameter"
+	echo "Usage: $0 local|pre-release|production"
+	exit 1
+	;;
+esac
+
 DIR=$(dirname `readlink -m $0`)
 cd $DIR/..; pwd
 
 # configuration of JVM
 JAVA_OPTS="-Xms512m -Xmx1g"
+echo "JAVA_OPTS: ${JAVA_OPTS}"
 
 # configuration of msgsender
-MSGSENDER_OPTS="--spring.config.location=config/msgsender.properties"
+MSGSENDER_OPTS="--spring.config.location=config/msgsender.properties,${ENV}.properties"
+echo "MSGSENDER_OPTS: ${MSGSENDER_OPTS}"
 
-exec java $JAVA_OPTS -jar target/msgsender.jar $MSGSENDER_OPTS
+if [ $ENV = local ]; then
+	echo "in the local environment"
+	exec java $JAVA_OPTS -jar target/msgsender.jar $MSGSENDER_OPTS
+else
+	echo "in the pre-release|production environment" 
+	exec java $JAVA_OPTS -jar libs/msgsender.jar $MSGSENDER_OPTS
+fi
