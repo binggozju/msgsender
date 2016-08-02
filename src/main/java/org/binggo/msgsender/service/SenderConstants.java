01e5624e79352b63e0ac4dd474fb03109163f346
binggo <binggozhan@163.com>
2016-08-02 19:43:09 +0800

    adjust the parameters of ThreadPoolTaskExecutor

--- src/main/java/org/binggo/msgsender/service/SenderConstants.java
+++ src/main/java/org/binggo/msgsender/service/SenderConstants.java
@@ -3,10 +3,10 @@ package org.binggo.msgsender.service;
 public final class SenderConstants {
 
 	// configuration for ThreadPoolTaskExecutor
-	public static int CORE_POOL_SIZE = 2;	// default: 1
-	public static int MAX_POOL_SIZE = 8;	//default: Integer.MAX_VALUE
+	public static int CORE_POOL_SIZE = 5;	// default: 1
+	public static int MAX_POOL_SIZE = 20;	//default: Integer.MAX_VALUE
 	public static int KEEPALIVE_SECONDS = 180;	// default: 60
-	public static int QUEUE_CAPACITY = 30;	// default: Integer.MAX_VALUE
+	public static int QUEUE_CAPACITY = 200;	// default: Integer.MAX_VALUE
 	
 	public static long SEND_TIMEOUT = 10; // second
 	public static int MAX_RETRY_TIMES = 3;
